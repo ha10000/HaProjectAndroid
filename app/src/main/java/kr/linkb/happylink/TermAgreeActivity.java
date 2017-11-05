82920564a25b8c0e0f6e8ca1e6cb32afe95b2082
ha jong soon <ha10000@mail.com>
2017-11-05 20:57:50 +0900

    first commit

---
+++ app/src/main/java/kr/linkb/happylink/TermAgreeActivity.java
@@ -0,0 +1,13 @@
+package kr.linkb.happylink;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TermAgreeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_term_agree);
+    }
+}
